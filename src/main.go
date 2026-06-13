e866e78e1aa147c9fd930fdfccde14a16c7fdb71
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2026-06-12 23:25:13 -0400

    feat: day 3

--- src/main.go
+++ src/main.go
@@ -1,14 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
-	var name string = "Mark"
-	var age int32 = 123
-	var heightMeters float32 = 3.5
-	var isDevops bool = true
+	// Day 2 - Variables, Mutability, and Types
+	// var name string = "Mark"
+	// var age int32 = 123
+	// var heightMeters float32 = 3.5
+	// var isDevops bool = true
 
-	fmt.Println(name, age, heightMeters, isDevops)
+	// fmt.Println(name, age, heightMeters, isDevops)
 
+	// Day 3 - FizzBuzz
+	iter := 20
+
+	for i := 1; i <= iter; i++ {
+		var output string = strconv.Itoa(i)
+
+		if i%3 == 0 {
+			output = "Fizz"
+		}
+		if i%5 == 0 {
+			output = "Buzz"
+		}
+		if i%3 == 0 && i%5 == 0 {
+			output = "FizzBuzz"
+		}
+
+		fmt.Println(output)
+	}
 }
