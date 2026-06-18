9ce7e068174debbce59e9cb785198c88f55b5100
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2026-06-18 09:43:32 -0400

    doc: add comments for each day

--- src/main.go
+++ src/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Server struct {
 	Name     string
 	CPUCores uint32
@@ -28,20 +26,20 @@ func (l Light) String() string {
 }
 
 func main() {
+	// Day 1 - Hello World & Toolchain
+	// fmt.Println("Hello, world!")
+
 	// Day 2 - Variables, Mutability, and Types
 	// var name string = "Mark"
 	// var age int32 = 123
 	// var heightMeters float32 = 3.5
 	// var isDevops bool = true
-
 	// fmt.Println(name, age, heightMeters, isDevops)
 
 	// Day 3 - FizzBuzz
 	// iter := 20
-
 	// for i := 1; i <= iter; i++ {
 	// 	var output string = strconv.Itoa(i)
-
 	// 	if i%3 == 0 {
 	// 		output = "Fizz"
 	// 	}
@@ -51,30 +49,26 @@ func main() {
 	// 	if i%3 == 0 && i%5 == 0 {
 	// 		output = "FizzBuzz"
 	// 	}
-
 	// 	fmt.Println(output)
 	// }
 
-	// Day 4
+	// Day 4 - Functions
 	// for i := 2; i <= 20; i++ {
 	// 	if isPrime(i) {
 	// 		fmt.Println(i)
 	// 	}
 	// }
 
-	// Day 5
+	// Day 5 - Collections — Slices, Strings
 	// var v []int
 	// for i := 1; i <= 10; i++ {
 	// 	v = append(v, i)
 	// }
-
 	// sum := 0
 	// for i := range v {
 	// 	sum += i
 	// }
-
 	// var avg float64 = float64(sum) / float64(len(v))
-
 	// var evens []int
 	// for i := range v {
 	// 	if i%2 == 0 {
@@ -83,12 +77,12 @@ func main() {
 	// }
 	// fmt.Printf("sum=%d, avg=%.2f, evens=%v", sum, avg, evens)
 
-	// Day 6
+	// Day 6 - Pointers
 	// s1 := "testing testing"
 	// s2 := "testing"
 	// fmt.Println(longest(s1, s2))
 
-	// Day 7
+	// Day 7 - Structs
 	// servers := []Server{
 	// 	{Name: "web-01", CPUCores: 4, IsActive: true},
 	// 	{Name: "web-02", CPUCores: 4, IsActive: false},
@@ -97,22 +91,21 @@ func main() {
 	// numActive := 0
 	// for _, s := range servers {
 	// 	fmt.Println(s.Name, "has", s.CPUCores, "cores")
-
 	// 	if s.IsActive {
 	// 		numActive++
 	// 	}
 	// }
 	// fmt.Println(numActive, "servers active")
 
-	// Day 7
-	lights := []Light{Red, Yellow, Green}
-	for _, l := range lights {
-		next := nextTrafficLight(l)
-
-		fmt.Println("Current:", l, "; Next:", next)
-	}
+	// Day 8 - Iota
+	// lights := []Light{Red, Yellow, Green}
+	// for _, l := range lights {
+	// 	next := nextTrafficLight(l)
+	// 	fmt.Println("Current:", l, "; Next:", next)
+	// }
 }
 
+// Day 4
 func isPrime(num int) bool {
 	if num < 2 {
 		return false
@@ -127,6 +120,7 @@ func isPrime(num int) bool {
 	return true
 }
 
+// Day 6
 func longest(s1, s2 string) int {
 	if len(s1) > len(s2) {
 		return len(s1)
@@ -134,6 +128,7 @@ func longest(s1, s2 string) int {
 	return len(s2)
 }
 
+// Day 8
 func nextTrafficLight(l Light) Light {
 	switch l {
 	case Red:
